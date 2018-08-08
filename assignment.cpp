fbfa773e100c0caea67b08085b6e86eb1fb8566a
AdyaAnwesa <42196590+AdyaAnwesa@users.noreply.github.com>
2018-08-08 09:55:36 +0530

    Add files via upload

---
+++ assignment.cpp
@@ -0,0 +1,11 @@
+//Library
+#include<iostream>
+
+//choose between multiple definations 
+using namespace std;
+
+//Integer function
+int main() {
+         cout << "Hello World, I'm Adya" ;
+           return 5;
+}
